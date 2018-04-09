48ef67fecef3bc7c8bfc57532c4a8db48a6a8f65
Sang Lee <sanglee.dev@gmail.com>
2018-04-09 00:52:53 -0700

    start the api server with PORT

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(8080);
+// Start the API server
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+  
